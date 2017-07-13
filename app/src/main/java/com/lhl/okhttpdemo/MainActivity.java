60279ce18d89823e8e249cb001dbac13e5c4c824
lihl <lihl1206@qq.com>
2017-07-13 10:55:14 +0800

    创建了一个字符串！

---
+++ app/src/main/java/com/lhl/okhttpdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.lhl.okhttpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void getAsynHttp(){
+        String string = "String";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
